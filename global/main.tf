b213b896faa4b3a100f5a38563d2fbda0af6b1a9
Veselin Ivanov <me@veselyn.com>
2025-09-15 08:01:25 +0200

    feat(global): version terraform-state bucket

--- global/main.tf
+++ global/main.tf
@@ -3,6 +3,17 @@ resource "github_repository" "infrastructure" {
   visibility = "private"
 }
 
-resource "aws_s3_bucket" "terraform_state" {
+module "s3_bucket_terraform_state" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "~> 5.7.0"
+
   bucket = "veselabs-terraform-state"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
 }
