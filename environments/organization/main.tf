6b9e33e43743d5015ed431cc92eed4b30a7e5f56
Veselin Ivanov <me@veselyn.com>
2025-10-02 22:41:46 +0200

    feat(organization): remove strangled gha-infrastructure role (#53)

--- environments/organization/main.tf
+++ environments/organization/main.tf
@@ -39,31 +39,6 @@ module "iam_oidc_provider" {
   url = "https://token.actions.githubusercontent.com"
 }
 
-module "iam_role_github_oidc" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role"
-  version = "~> 6.2.1"
-
-  enable_github_oidc = true
-
-  name            = "gha-infrastructure"
-  use_name_prefix = false
-
-  oidc_wildcard_subjects = ["veselabs/infrastructure:*"]
-
-  policies = {
-    AdministratorAccess = "arn:aws:iam::aws:policy/AdministratorAccess"
-  }
-
-  create_inline_policy = true
-  inline_policy_permissions = {
-    DenyPlanUpgrade = {
-      effect    = "Deny",
-      actions   = ["freetier:UpgradeAccountPlan"]
-      resources = ["*"]
-    }
-  }
-}
-
 module "iam_role_github_oidc_read" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role"
   version = "~> 6.2.1"
