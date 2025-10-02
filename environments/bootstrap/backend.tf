95876f8d644419b8d93b6769104178f7f9350cd0
Veselin Ivanov <me@veselyn.com>
2025-10-02 21:24:55 +0200

    feat(bootstrap): extract state S3 bucket to own environment

--- environments/bootstrap/backend.tf
+++ environments/bootstrap/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     region       = "eu-north-1"
     bucket       = "veselabs-terraform-state"
-    key          = "development/terraform.tfstate"
+    key          = "bootstrap/terraform.tfstate"
     use_lockfile = true
   }
 }
